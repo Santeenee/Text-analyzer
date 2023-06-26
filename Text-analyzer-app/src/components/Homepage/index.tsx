4855c9bc021ae4cf5c9dc73fb5d864bd22a7605e
Francesco Santini <francescotn02@gmail.com>
2023-06-26 18:35:11 +0200

    Better responsiveness (now OK on mobile)

--- Text-analyzer-app/src/components/Homepage/index.tsx
+++ Text-analyzer-app/src/components/Homepage/index.tsx
@@ -1,11 +1,11 @@
-import { Grid, Page, Text, Textarea } from '@geist-ui/core'
+import { Grid, Page, Text, Textarea, useMediaQuery } from '@geist-ui/core'
 import { useState, useEffect } from 'react'
 
 export default function Homepage() {
 	const [textAreaValue, setTextAreaValue] = useState<string>('')
 	const [wordCount, setWordCount] = useState<number>(0)
 	const [charactersCount, setCharactersCount] = useState<number>(0)
-	// const isXS = useMediaQuery('xs')
+	const isXS = useMediaQuery('xs', { match: 'down' })
 
 	useEffect(() => {
 		countWords(textAreaValue)
@@ -32,7 +32,7 @@ export default function Homepage() {
 	}
 
 	return (
-		<Page>
+		<Page style={isXS ? { margin: 0, width: '100%' } : {}}>
 			<Grid.Container
 				gap={1}
 				alignItems="baseline"
@@ -44,7 +44,7 @@ export default function Homepage() {
 						Text Analyzer
 					</Text>
 				</Grid>
-				<Grid.Container xs={24} sm md={12} gap={1} h="max-content">
+				<Grid.Container xs={24} sm md={12} gap={1} h="max-content" ml="0">
 					<Grid xs={24} md={12}>
 						<Text p width="100%" margin={0}>
 							{wordCount} words
@@ -61,7 +61,7 @@ export default function Homepage() {
 			<Textarea
 				resize="vertical"
 				w="100%"
-				style={{ height: '60dvh', fontSize: '1rem' }}
+				style={{ height: '60dvh', maxHeight: '80dvh', fontSize: '1rem' }}
 				placeholder="Please enter some text"
 				onChange={handleChange}
 				value={textAreaValue}
